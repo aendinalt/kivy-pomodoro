90f26b5527d927b29d3fff69b16b9f1aa8d5dabd
Andrey Matukhno <andrey.matukhno@altexsoft.com>
2015-04-06 17:15:23 +0300

    add media\n create basic static UI

---
+++ kivytomato.py
@@ -0,0 +1,20 @@
+__author__ = 'Aen Dinalt'
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
+
+
+class TomatoLayout(FloatLayout):
+    pass
+
+
+class TomatoApp(App):
+    def build(self):
+        Config.set('graphics', 'width', '400')
+        Config.set('graphics', 'height', '400')
+        return TomatoLayout()
+
+if __name__ == '__main__':
+    TomatoApp().run()
