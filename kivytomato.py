7dc6d9f8e0f1e781b74b4d3063d1c3f5d5c16005
Andrey Matukhno <andrey.matukhno@altexsoft.com>
2015-04-10 11:53:50 +0300

    add start and stop icons

--- kivytomato.py
+++ kivytomato.py
@@ -15,6 +15,21 @@ class LargeIconWidget(Widget):
         super(LargeIconWidget, self).__init__(**kwargs)
 
 
+class StartButton(Widget):
+    def __init__(self, **kwargs):
+        super(StartButton, self).__init__(**kwargs)
+
+
+class StopButton(Widget):
+    def __init__(self, **kwargs):
+        super(StopButton, self).__init__(**kwargs)
+
+
+class ClearButton(Widget):
+    def __init__(self, **kwargs):
+        super(ClearButton, self).__init__(**kwargs)
+
+
 class TomatoApp(App):
     def build(self):
         Config.set('graphics', 'width', '400')
